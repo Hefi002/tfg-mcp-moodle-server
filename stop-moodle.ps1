4030f5baf4caba834aca94c5b70ddc5fb14b53ae
Hefi002 <eerreroe@gmail.com>
2026-01-09 19:30:27 +0100

    Removal of some dev files. This should be the last commit before "release"

--- stop-moodle.ps1
+++
@@ -1,7 +0,0 @@
-#Script to stop Moodle Docker containers (Linux)
-
-$env:MOODLE_DOCKER_WWWROOT = "$PSScriptRoot\dev-environment\moodle"
-$env:MOODLE_DOCKER_DB = "pgsql"
-
-Set-Location "$PSScriptRoot\dev-environment\moodle-docker"
-.\bin\moodle-docker-compose stop
